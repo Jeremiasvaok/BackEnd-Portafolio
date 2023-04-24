86364bbdbccc3e001cceaa2b16c2aa430a2584f8
Jeremias Vallejos Gonzalez <jeremias.vallejos78@gmail.com>
2023-04-24 18:33:10 -0300

    primer commit

---
+++ JV/JV/src/test/java/com/portafolioWeb/JV/JvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portafolioWeb.JV;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
