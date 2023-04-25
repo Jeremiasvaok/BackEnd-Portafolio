da94a1e577a2f751a64c0b31acec2ef0c63e672d
Jeremias Vallejos Gonzalez <jeremias.vallejos78@gmail.com>
2023-04-24 21:01:25 -0300

    subir

---
+++ JeremiasV/JeremiasV/src/main/java/com/portafolioWeb/JeremiasV/JeremiasVApplication.java
@@ -0,0 +1,13 @@
+package com.portafolioWeb.JeremiasV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeremiasVApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeremiasVApplication.class, args);
+	}
+
+}
---
+++ JeremiasV/JeremiasV/src/test/java/com/portafolioWeb/JeremiasV/JeremiasVApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portafolioWeb.JeremiasV;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeremiasVApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
